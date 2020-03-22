f508aa67db6dc8a7b37828d13fd384b019abb383
akrammel <akrammmel@gmail.com>
2020-03-22 23:13:43 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// Install BootstrapVue
+import { BootstrapVue} from 'bootstrap-vue'
+
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
