22ff35be91d5c474110b7dc8d0be7850d066151b
akrammel <akrammmel@gmail.com>
2020-03-25 00:24:02 +0100

    beauty

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,84 @@
 <template>
-<div id="app">
+  <div id="app">
     <b-navbar fixed="top" type="light" variant="light">
       <b-navbar-brand class="title">vuejs-pharmacies-garde</b-navbar-brand>
     </b-navbar>
     <b-container style="margin-top: 100px;margin-bottom: 100px;">
-    <h1>Pharmacies de garde Algérie</h1>
-    <b-row class="justify-content-md-center">
-      <b-col md="6">
-        <b-row>
-          <b-col md="12" class="text-center">
-            <b-form-group class="text-left">
-              <label>Choisir la wilaya</label>
-              <b-form-select @change="chargercommunes" v-model="wilayaChosis">
-                <b-form-select-option v-for="(option, index) in wilayas" v-bind:key="index" v-bind:value="option.id">
-                  {{ option.wilaya }}
-                </b-form-select-option>
-              </b-form-select>
-            </b-form-group>
-          </b-col>
-          <b-col md="12" class="text-left">
-            <label>Choisir la où les communes</label>
-            <div class="btn-grp">
-            <b-button @click="checkAll" variant="primary" v-if="communesnow.length != 0">Tout chocher</b-button>
-            <b-button @click="uncheckAll" variant="outline-primary" v-if="communesnow.length != 0">Tout décohocher</b-button>
-            </div>
+      <h1>Pharmacies de garde Algérie</h1>
+      <b-row class="justify-content-md-center">
+        <b-col md="6">
+          <b-row>
+            <b-col md="12" class="text-center">
+              <b-form-group class="text-left">
+                <label>Choisir la wilaya</label>
+                <b-form-select @change="chargercommunes" v-model="wilayaChosis">
+                  <b-form-select-option
+                    v-for="(option, index) in wilayas"
+                    v-bind:key="index"
+                    v-bind:value="option.id"
+                  >{{ option.wilaya }}</b-form-select-option>
+                </b-form-select>
+              </b-form-group>
+            </b-col>
+            <b-col md="12" class="text-left">
+              <label>Choisir la où les communes</label>
+              <div class="btn-grp">
+                <b-button
+                  @click="checkAll"
+                  variant="primary"
+                  v-if="communesnow.length != 0"
+                >Tout chocher</b-button>
+                <b-button
+                  @click="uncheckAll"
+                  variant="outline-primary"
+                  v-if="communesnow.length != 0"
+                >Tout décohocher</b-button>
+              </div>
               <b-form-group>
-                <b-form-checkbox-group
-                  v-model="communesChoisis"
-                >
-                <b-form-checkbox
-                 v-for="(option, index) in communesnow" v-bind:key="index" v-bind:value="option.id"
-              >
-                {{option.commune}}
-              </b-form-checkbox>
+                <b-form-checkbox-group v-model="communesChoisis">
+                  <b-form-checkbox
+                    v-for="(option, index) in communesnow"
+                    v-bind:key="index"
+                    v-bind:value="option.id"
+                  >{{option.commune}}</b-form-checkbox>
                 </b-form-checkbox-group>
               </b-form-group>
+            </b-col>
+            <!--<b-col md="12" class="text-center"><b-button variant="outline-primary" style="width: 100%">Envoyer</b-button></b-col>-->
+          </b-row>
         </b-col>
-        <!--<b-col md="12" class="text-center"><b-button variant="outline-primary" style="width: 100%">Envoyer</b-button></b-col>-->
-        </b-row>
-      </b-col>
-      <b-col md="6">
-        <Pharmacies v-if="$data.wilayaChosis.length != 0 && $data.communesChoisis.length != 0" :wilaya="$data.wilayaChosis" :communes="$data.communesChoisis"/>
-      </b-col>  
-    </b-row>
-  </b-container>
-<footer class="text-center">
-  <div class="row h-100">
-    <div class="col-sm-12 my-auto">
-        <span>Check out the <a href="https://github.com/akrammel/vuejs-pharmacies-garde">Repository</a> on Github.</span>
-    </div>
+        <b-col md="6">
+          <Pharmacies
+            v-if="$data.wilayaChosis.length != 0 && $data.communesChoisis.length != 0"
+            :wilaya="$data.wilayaChosis"
+            :communes="$data.communesChoisis"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+    <footer class="text-center">
+      <div class="row h-100">
+        <div class="col-sm-12 my-auto">
+          <span>
+            Check out the
+            <a href="https://github.com/akrammel/vuejs-pharmacies-garde">Repository</a> on Github.
+          </span>
+        </div>
+      </div>
+    </footer>
   </div>
-</footer>
-</div>
 </template>
 
 <script>
-import Pharmacies from './components/Pharmacies.vue'
+import Pharmacies from "./components/Pharmacies.vue";
 import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Pharmacies
   },
-  data: function () {
+  data: function() {
     return {
       wilayas: [],
       communes: [],
@@ -71,55 +86,56 @@ export default {
       wilayaChosis: "16",
       communesChoisis: [],
       year: ""
-    }
-  },created () {
-    this.year = new Date().getFullYear(); 
+    };
+  },
+  created() {
+    this.year = new Date().getFullYear();
     axios
-      .get('../../wilayas.json')
+      .get("../../wilayas.json")
       .then(response => (this.wilayas = response.data));
     axios
-      .get('../../communes.json')
-      .then(response => (this.communes = response.data)).then(this.chargercommunes);
-  },mounted () {
-
-  }
-  ,methods: {
-    chargercommunes: function () {
+      .get("../../communes.json")
+      .then(response => (this.communes = response.data))
+      .then(this.chargercommunes);
+  },
+  mounted() {},
+  methods: {
+    chargercommunes: function() {
       this.communesChoisis = [];
       var s = [];
       var w = "";
       this.wilayas.forEach(element => {
-        if(element.id == this.wilayaChosis){
+        if (element.id == this.wilayaChosis) {
           w = element.wilaya;
-      }
+        }
       });
       this.communes.forEach(element => {
-        if(element.wilaya == w){
-          s.push({commune: element.commune, id: element.id})
-      }
-      this.communesnow = s;
-      });  
+        if (element.wilaya == w) {
+          s.push({ commune: element.commune, id: element.id });
+        }
+        this.communesnow = s;
+      });
     },
-    checkAll: function () {
+    checkAll: function() {
       var s = [];
       var w = "";
       this.wilayas.forEach(element => {
-        if(element.id == this.wilayaChosis){
+        if (element.id == this.wilayaChosis) {
           w = element.wilaya;
-      }
+        }
       });
       this.communes.forEach(element => {
-        if(element.wilaya == w){
-          s.push(element.id)
-      }
-      this.communesChoisis = s;
-      });  
+        if (element.wilaya == w) {
+          s.push(element.id);
+        }
+        this.communesChoisis = s;
+      });
     },
-    uncheckAll: function(){
-      this.communesChoisis = []
+    uncheckAll: function() {
+      this.communesChoisis = [];
     }
   }
-}
+};
 </script>
 
 <style>
@@ -127,34 +143,34 @@ export default {
   font-size: 16px;
 }
 .navbar-brand {
-  margin-right: auto!important;
-  margin-left: auto!important;
+  margin-right: auto !important;
+  margin-left: auto !important;
 }
 .custom-control-inline {
-  margin-right: 15px!important;
-  margin-bottom: 5px!important;
-  font-size: 14px!important;
-  line-height: 1.8!important;
+  margin-right: 15px !important;
+  margin-bottom: 5px !important;
+  font-size: 14px !important;
+  line-height: 1.8 !important;
 }
 .btn-grp {
   margin-bottom: 15px;
 }
 .btn-grp > button {
-  margin-right: 10px!important;
+  margin-right: 10px !important;
 }
 body {
-  font-size: 16px!important;
+  font-size: 16px !important;
 }
 table > tbody {
-  font-size: 14px!important;
-  text-transform: uppercase!important;
+  font-size: 14px !important;
+  text-transform: uppercase !important;
 }
 footer {
-  position: fixed!important;
+  position: fixed !important;
   bottom: 0;
   background-color: #f8f9fa;
   height: 40px;
   width: 100%;
-  font-size: 12px!important;
+  font-size: 12px !important;
 }
 </style>
--- src/components/Pharmacies.vue
+++ src/components/Pharmacies.vue
@@ -1,32 +1,36 @@
 <template>
-<div class="row h-100 text-center">
-  <div class="col-sm-12 my-auto">
-      <b-spinner style="width: 5rem; height: 5rem;" class="align-middle" v-if="result.length == 0" variant="primary" type="grow" label="Chargement.."></b-spinner>        
-      <b-table-simple v-else
-      responsive
-      style="margin-bottom: 50px;"
-      >
-      <b-thead>
-        <b-tr>
-          <b-th>Date</b-th>
-          <b-th>Heure</b-th>
-          <b-th>Commune</b-th>
-          <b-th>Pharmacien</b-th>
-          <b-th>Adresse</b-th>
-        </b-tr>
+  <div class="row h-100 text-center">
+    <div class="col-sm-12 my-auto">
+      <b-spinner
+        style="width: 5rem; height: 5rem;"
+        class="align-middle"
+        v-if="result.length == 0"
+        variant="primary"
+        type="grow"
+        label="Chargement.."
+      ></b-spinner>
+      <b-table-simple v-else responsive style="margin-bottom: 50px;">
+        <b-thead>
+          <b-tr>
+            <b-th>Date</b-th>
+            <b-th>Heure</b-th>
+            <b-th>Commune</b-th>
+            <b-th>Pharmacien</b-th>
+            <b-th>Adresse</b-th>
+          </b-tr>
         </b-thead>
         <b-tbody>
-        <b-tr v-for="(item, index) in result" v-bind:key="index">
-          <b-td width="100">{{item[0]}}</b-td>
-          <b-td width="100">{{item[1]}}</b-td>
-          <b-td>{{item[2]}}</b-td>
-          <b-td>{{item[3]}}</b-td>
-          <b-td>{{item[4]}}</b-td>
-        </b-tr>
+          <b-tr v-for="(item, index) in result" v-bind:key="index">
+            <b-td width="100">{{item[0]}}</b-td>
+            <b-td width="100">{{item[1]}}</b-td>
+            <b-td>{{item[2]}}</b-td>
+            <b-td>{{item[3]}}</b-td>
+            <b-td>{{item[4]}}</b-td>
+          </b-tr>
         </b-tbody>
       </b-table-simple>
-  </div>   
-</div>  
+    </div>
+  </div>
 </template>
 
 <script>
@@ -34,59 +38,64 @@ import axios from "axios";
 import qs from "qs";
 
 export default {
-  name: 'Pharmacies',
+  name: "Pharmacies",
   props: {
     wilaya: String,
     communes: Array
   },
-  data: function () {
+  data: function() {
     return {
       result: []
-    }
+    };
   },
-  created(){
+  created() {
     var result = [];
-    var today = new Date(); 
-    var dd = today.getDate(); 
-    var mm = today.getMonth() + 1; 
-    var yyyy = today.getFullYear(); 
-    if (dd < 10) { 
-      dd = '0' + dd; 
-    } 
-    if (mm < 10) { 
-      mm = '0' + mm; 
-    } 
-    today = dd + '-' + mm + '-' + yyyy;
-    axios.post('https://cors-anywhere.herokuapp.com/http://www.sante.gov.dz/garde/garde.php', qs.stringify({
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    today = dd + "-" + mm + "-" + yyyy;
+    axios
+      .post(
+        "https://cors-anywhere.herokuapp.com/http://www.sante.gov.dz/garde/garde.php",
+        qs.stringify({
           wilay2: this.wilaya,
-          valide: 'true',
-          wil: 'true',
+          valide: "true",
+          wil: "true",
           options: this.communes
-        }),{
-          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        })
-        .then(function (response) {
-          let doc = new DOMParser().parseFromString(response.data, 'text/html');
-          let tbody = doc.body.querySelectorAll('tbody')[3];
-          let trs = tbody.querySelectorAll('tr');
-          (trs).forEach(element => {
-            var r = [];
-            if(element.querySelectorAll('td')[0].textContent.trim() == today){
-              element.querySelectorAll('td').forEach(td => {
-                r.push(td.textContent.trim().replace('\\', ""))
-              });
-            }
-            if(r.length != 0){
-              result.push(r);
-              r = [];
-            }
-          }); 
-        }).then(this.result = result).then(result = []);
-        
+        }),
+        {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        }
+      )
+      .then(function(response) {
+        let doc = new DOMParser().parseFromString(response.data, "text/html");
+        let tbody = doc.body.querySelectorAll("tbody")[3];
+        let trs = tbody.querySelectorAll("tr");
+        trs.forEach(element => {
+          var r = [];
+          if (element.querySelectorAll("td")[0].textContent.trim() == today) {
+            element.querySelectorAll("td").forEach(td => {
+              r.push(td.textContent.trim().replace("\\", ""));
+            });
+          }
+          if (r.length != 0) {
+            result.push(r);
+            r = [];
+          }
+        });
+      })
+      .then((this.result = result))
+      .then((result = []));
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
