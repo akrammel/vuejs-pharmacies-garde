02ccd591392a5ce2c29bdb15722331b7f9693bef
akrammel <akrammmel@gmail.com>
2020-03-29 11:54:23 +0100

    faster

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue} from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
+import { TablePlugin } from 'bootstrap-vue'
+import { SpinnerPlugin } from 'bootstrap-vue'
+import { LayoutPlugin } from 'bootstrap-vue'
+import { FormGroupPlugin } from 'bootstrap-vue'
+import { FormSelectPlugin } from 'bootstrap-vue'
+import { FormCheckboxPlugin } from 'bootstrap-vue'
+import { ButtonPlugin } from 'bootstrap-vue'
+import { NavbarPlugin } from 'bootstrap-vue'
+Vue.use(NavbarPlugin)
+Vue.use(FormCheckboxPlugin)
+Vue.use(ButtonPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(LayoutPlugin)
+Vue.use(SpinnerPlugin)
+Vue.use(TablePlugin)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
